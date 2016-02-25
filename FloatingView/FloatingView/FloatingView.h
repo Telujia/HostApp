7668f0fbc02ffd8d1bf19e08a3fb2aaec2a62c4b
Chazz Lu <lucaiguang@ChazzLu.local>
2016-02-26 00:18:27 +0800

    add FloatingView Framework.

---
+++ FloatingView/FloatingView/FloatingView.h
@@ -0,0 +1,19 @@
+//
+//  FloatingView.h
+//  FloatingView
+//
+//  Created by lucaiguang on 16/2/26.
+//  Copyright © 2016年 Chazz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatingView.
+FOUNDATION_EXPORT double FloatingViewVersionNumber;
+
+//! Project version string for FloatingView.
+FOUNDATION_EXPORT const unsigned char FloatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingView/PublicHeader.h>
+
+

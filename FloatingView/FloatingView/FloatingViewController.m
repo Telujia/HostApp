b2622d4ff311584cc3de5a395f27bd300b2af46a
Chazz Lu <lucaiguang@ChazzLu.local>
2016-06-13 13:04:27 +0800

    add
    add awesomeproject

--- FloatingView/FloatingView/FloatingViewController.m
+++ FloatingView/FloatingView/FloatingViewController.m
@@ -10,31 +10,59 @@
 
 @interface FloatingViewController ()
 @property (nonatomic, strong) UIView *maskView;
+@property (nonatomic, strong) UIView *bugAlert;
 @end
 
 @implementation FloatingViewController
 
 - (UIImageView *)imageView {
     if (!_imageView) {
-        _imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
+        _imageView = [[UIImageView alloc] init];
     }
     return _imageView;
 }
 
 - (UIView *)maskView {
     if (!_maskView) {
-        _maskView = [[UIView alloc] initWithFrame:self.view.bounds];
-        _maskView.backgroundColor = [UIColor blueColor];
+        _maskView = [[UIView alloc] init];
+        _maskView.backgroundColor = [UIColor blackColor];
         _maskView.alpha = 0.6;
     }
     return _maskView;
 }
 
+- (UIView *)bugAlert {
+    if (!_bugAlert) {
+        _bugAlert = [[UIView alloc] init];
+        _bugAlert.backgroundColor = [UIColor redColor];
+    }
+    return _bugAlert;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     self.view.backgroundColor = [UIColor redColor];
     [self.view addSubview:self.imageView];
+    self.imageView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.imageView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.f constant:0.f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.imageView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.f constant:0.f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.imageView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.f constant:0.f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.imageView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1.f constant:0.f]];
+    
     [self.view addSubview:self.maskView];
+    self.maskView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.maskView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.f constant:0.f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.maskView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.f constant:0.f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.maskView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.f constant:0.f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.maskView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1.f constant:0.f]];
+    
+    [self.maskView addSubview:self.bugAlert];
+    self.bugAlert.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.maskView addConstraint:[NSLayoutConstraint constraintWithItem:self.bugAlert attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.maskView attribute:NSLayoutAttributeCenterX multiplier:1.f constant:0.f]];
+    [self.maskView addConstraint:[NSLayoutConstraint constraintWithItem:self.bugAlert attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.maskView attribute:NSLayoutAttributeCenterY multiplier:1.f constant:0.f]];
+    [self.bugAlert addConstraint:[NSLayoutConstraint constraintWithItem:self.bugAlert attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.f constant:200.f]];
+    [self.bugAlert addConstraint:[NSLayoutConstraint constraintWithItem:self.bugAlert attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.f constant:120.f]];
 }
 
 @end
